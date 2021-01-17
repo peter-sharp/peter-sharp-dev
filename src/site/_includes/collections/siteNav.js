61101de878078a20164200b567ae0f0e6040ab14
Peter Sharp <peter.victor.sharp@gmail.com>
2021-01-17 22:58:37 +1300

    Fixes higher indices showing before lower

--- src/site/_includes/collections/siteNav.js
+++ src/site/_includes/collections/siteNav.js
@@ -1,5 +1,7 @@
 module.exports = function siteNavCollection(collection) {
-    return collection.getFilteredByTag('page').sort(function(a, b) {
+    const items = collection.getFilteredByTag('page').slice();
+    
+    return items.sort(function(a, b) {
         return b.data.menuIndex - a.data.menuIndex
-    })
+    }).reverse();
 }
