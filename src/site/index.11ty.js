289a247ce9ce8bf8510248219a44f63ea574e1cf
Peter Sharp <peter.victor.sharp@gmail.com>
2021-03-27 17:20:02 +1300

    Capitalises titles

--- src/site/index.11ty.js
+++ src/site/index.11ty.js
@@ -15,7 +15,7 @@ module.exports.render = function index() {
   </header>
   <section id="services" class="wrapper">
     <div class="wrapper__inner p-note stack stack--gap-100">
-      <h2>services</h2>
+      <h2>Services</h2>
       <ul>
         <li>Web apps</li>
         <li>Brochure websites</li>
@@ -25,7 +25,7 @@ module.exports.render = function index() {
   </section>
   <section id="contactUs" class="wrapper">
     <div class="wrapper__inner p-note stack stack--gap-100">
-        <h2>about</h2>
+        <h2>About</h2>
         <p>Fullstack web developer living in <span class="p-locality">Fairlie</span>, <span class="p-region">South Canterbury</span>, <span class="p-country-name">New Zealand</span> with 4+ years experience designing, delivering and maintaining small to large scale websites and web apps in WordPress as well as a variety of other frameworks.
         </p>
         <p>Being part designer, I care about crafting sites and apps with good user experience that excel in meeting their bottom line.</p>
