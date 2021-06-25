5d23badfebcd616cc3ab2bf589403dbbbd47f610
Peter Sharp <hello@petersharp.dev>
2021-06-25 09:50:15 -0600

    Adds honeypot

--- src/site/contact/index.11ty.js
+++ src/site/contact/index.11ty.js
@@ -7,7 +7,7 @@ module.exports.data = {
 
 module.exports.render = function contact() {
   return /*html*/`
-    <form name="contact" netlify action="/contact/success" class="wrapper pad-top-900">
+    <form name="contact" netlify action="/contact/success" netlify-honeypot="address" class="wrapper pad-top-900">
       <div class="wrapper__inner stack stack--gap-100">
         <h1>Contact</h1>
         <p class="form-group">
@@ -18,9 +18,13 @@ module.exports.render = function contact() {
           <label for="contactEmail">email</label>
           <input type="email" required id="contactEmail" name="email">
         </p>
-        <p class="form-group">
+        <p class="form-group hide-next">
           <label for="contactMessage">message</label>
           <textarea required id="contactMessage" name="message"></textarea>
+        </p>
+        <p class="form-group" >
+          <label for="contactAddress">address - don't fill out this field if you're a human</label>
+          <textarea id="contactAddress" name="address"></textarea>
         </p>
         <p>
           <label><input type="checkbox" id="contactPromotionalContent" name="promotionalContent"> Send me promotional content</label>
