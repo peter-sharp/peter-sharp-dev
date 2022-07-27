a20dd8070c733bd8308e95a8a44175ec59e5a71c
Peter Sharp <hello@petersharp.dev>
2022-07-26 20:42:40 -0600

    Fixes Gallery not showing image for internal links

--- src/site/_includes/components/gallery.js
+++ src/site/_includes/components/gallery.js
@@ -1,8 +1,10 @@
+const slugify = require('slugify')
+
 module.exports = function gallery(collection, size = '', justify = '', local = false) {
     return /*html*/`<ul class="gallery" ${size && `size="${size}"`} ${justify && `justify="${justify}"`}>
         ${collection.map(x => /*html*/`<li class="card gallery__card">
         <figure class="stack">
-          <img class="card__image" width="500" height="281" src="${local ? '' : '/assets/'}${x.data.link}.jpg"/>
+          <img class="card__image" width="500" height="281" src="${local ? '' : '/assets/'}${slugify(x.data.link)}.jpg"/>
           <figcaption class="card__title">
             <strong><a class="card__link" href="https://${x.data.link}">${x.data.title}</a></strong>
             <span>${x.data.subtitle}</span>
