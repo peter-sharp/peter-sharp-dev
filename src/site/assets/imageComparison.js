7571d4eb0bd51cc13455c5d2187114a136c6e012
Peter Sharp <hello@petersharp.dev>
2022-08-25 13:47:36 -0600

    Adds fix for offsetX in FF

--- src/site/assets/imageComparison.js
+++ src/site/assets/imageComparison.js
@@ -8,7 +8,10 @@ import throttle from '/assets/throttle.js';
 document.addEventListener('pointermove', 
     delegate('[data-image-comparison]', throttle(function imageComparison(ev) {
         const rect = this.getBoundingClientRect();
-        const percent = ev.offsetX / rect.width * 100;
-        console.log(ev.offsetX, rect.width, percent);
+        
+        // Fix for FF
+        const offsetX = ev.offsetX || ev.clientX - rect.left;
+
+        const percent = offsetX / rect.width * 100;
         this.style.setProperty('--percent', `${percent}%`);
 })));
